c258e142913706b1b35a19125068b2073b3e40c3
John Brian Kirby <jbkirby@users.noreply.github.com>
2014-11-11 10:56:14 -0800

    Output test result to CSS. Standardized on double quotations.

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
-var imgToDataUri = require('../index.js');
-var os = require('os');
-var path = require('path');
-var fs = require('fs');
+var imgToDataUri = require("../index.js");
+var os = require("os");
+var path = require("path");
+var fs = require("fs");
 
 var inputFile = path.join( __dirname, "test_1.css" );
 fs.createReadStream(inputFile)
 	.pipe(imgToDataUri(inputFile, { maxImageSize: 32768 }))
-	.pipe(process.stdout);
+	.pipe(fs.createWriteStream("./test_1_result.css"));
